519b9ce017bd1f9069f16de251b7f913be9c1fb0
osaid500 <redfirerpk@gmail.com>
2024-02-03 04:06:03 +0300

    Create simple navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
+    <div className="bg-slate-200 min-h-screen">
       <Navbar />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Navbar() {
-  return <header>Navbar</header>;
+  return (
+    <header className="py-2 px-10 flex justify-between items-center bg-white">
+      <div className="flex items-center gap-1 text-gray-700 cursor-default transition hover:text-teal-700">
+        <i class="fa-regular fa-circle-user text-xl"></i>
+        <h1 className="text-2xl font-medium">Kewbuzz</h1>
+      </div>
+
+      <ul>
+        <li>
+          <a className="transition hover:text-cyan-800" href="/">
+            Home
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
 }
 
 export default Navbar;
