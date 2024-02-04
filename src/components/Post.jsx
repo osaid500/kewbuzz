9c1f5926023a1f10d99d7c7c8b802ba4511e4aa7
osaid500 <redfirerpk@gmail.com>
2024-02-04 03:54:01 +0300

    Implement Hover and Focus Effects, improve accessibility.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,12 +5,23 @@ function Post() {
     <article className="bg-white w-full flex gap-6 font-inter rounded-md p-4">
       <div className="flex-col w-full">
         <div className="flex items-center gap-4 mb-1 h-10">
-          <span className="font-medium text-emerald-700">main_community</span>
+          <a
+            className="font-medium text-emerald-700 hover:text-teal-900 focus:text-teal-900"
+            href="#"
+          >
+            main_community
+          </a>
           <span className="text-gray-600 text-xs mt-0.5">
-            Posted by <span className="text-cyan-700 font-bold">osaid500</span>
+            Posted by{" "}
+            <a
+              className="font-bold text-cyan-700 hover:text-cyan-900 focus:text-cyan-900"
+              href="#"
+            >
+              osaid500
+            </a>
           </span>
         </div>
-        <div className="">
+        <div>
           <h3 className="text-lg font-medium mb-2">
             I found some free resources for stock images
           </h3>
@@ -23,15 +34,15 @@ function Post() {
         </div>
       </div>
       <div className="flex flex-col justify-center items-center p-">
-        <div className="w-full">
+        <div className="w-full flex flex-col gap-1">
           <div className="flex items-center">
-            <button className="mr-3">
+            <button className="mr-3 transition hover:text-teal-600 focus:text-green-700">
               <i class="fa-solid fa-arrow-up text-lg"></i>
             </button>
             <p className="text-xs text-green-600">39.5k</p>
           </div>
           <div className="flex items-center">
-            <button className="mr-3">
+            <button className="mr-3 transition hover:text-red-600 focus:text-red-700">
               <i class="fa-solid fa-arrow-down text-lg"></i>
             </button>
             <p className="text-xs text-red-700">4.6k</p>
